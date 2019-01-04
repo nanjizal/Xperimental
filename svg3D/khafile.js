25973af1ca93932ca431c4ab55300147f00b759c
nanjizal <nanjizal@users.noreply.github.com>
2019-01-04 04:02:32 +0000

    remove polyK dependancy as no longer needed.

--- svg3D/khafile.js
+++ svg3D/khafile.js
@@ -1,13 +1,9 @@
 let project = new Project('Empty');
-
 project.addSources('Sources');
 project.addShaders('Shaders');
 project.addLibrary('iron');
 project.addLibrary('trilateral');
 project.addLibrary('trilateralXtra');
-project.addLibrary('hxPolyK');
-//project.addLibrary('poly2trihx');
 project.addLibrary('hxGeomAlgo');
 project.addAssets('Assets/**');
-
 resolve(project);
