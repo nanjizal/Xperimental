0e3d7b4308f17bb5ca1ddb108e58760d507f2fdb
nanjizal <nanjizal@users.noreply.github.com>
2019-01-26 20:52:51 +0000

    added comments

--- inventory/MainInventory.hx
+++ inventory/MainInventory.hx
@@ -3,9 +3,9 @@ import nanjizal.inventoryShopify.Merger;
 class MainInventory{
     public static function main(){ 
         var args = Sys.args();
-        Merger.mergeHandlesInto( args[0]
-                               , args[1]
-                               , args[2]
-                               , args[3] );
+        Merger.mergeHandlesInto( args[0]      // file to change
+                               , args[1]      // file with products to copy over
+                               , args[2]      // 'handles' or products that should be copied over
+                               , args[3] );   // file to save inventory to
     }
 }
